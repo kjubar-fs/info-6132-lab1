36d15d472bac95ebb3bda8fd122d7369031af0e9
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-10-02 13:56:28 -0400

    Tiny safe area padding tweak

--- src/includes/globalStyles.js
+++ src/includes/globalStyles.js
@@ -1,12 +1,12 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 2 Oct 2024, 12:14:20 PM
- *  Last update: 2 Oct 2024, 12:17:52 PM
+ *  Last update: 2 Oct 2024, 1:56:19 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { Platform } from "react-native"
 
-export const safeAreaPadding = Platform.OS === "ios" ? 60 : 50;
+export const safeAreaPadding = Platform.OS === "ios" ? 60 : 45;
 
 export const rootColor = "#5296A5";
 export const accentColor = "#82DDF0";
